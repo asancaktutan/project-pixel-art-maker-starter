143e84e981a881cf4b66d3e2ccc5e21ded93a55b
asancaktutan <74071154+asancaktutan@users.noreply.github.com>
2020-11-06 22:49:24 +0300

    implement the script

--- designs.js
+++ designs.js
@@ -3,8 +3,38 @@
 
 // When size is submitted by the user, call makeGrid()
 
+let currentColor = '#000000';
+let height, width;
+const form = document.getElementById('sizePicker');
+const colorPicker = document.getElementById('colorPicker');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  height = document.getElementById('inputHeight').value;
+  width = document.getElementById('inputWidth').value;
+  makeGrid();
+});
+
+colorPicker.addEventListener('change', (e) => {
+  currentColor = e.currentTarget.value;
+});
+
+
+
 function makeGrid() {
-
-// Your code goes here!
-
+  const pixelCanvas = document.getElementById('pixelCanvas');
+  pixelCanvas.innerHTML = '';
+  const tbody = document.createElement('tbody');
+  for (let i = 0;i < height;i ++) {
+    const row = document.createElement('tr');
+    for (let j = 0;j < width;j ++) {
+      const cell = document.createElement('td');
+      cell.addEventListener('click', (e) => {
+        e.currentTarget.style.backgroundColor = currentColor;
+      })
+      row.appendChild(cell);
+    }
+    tbody.appendChild(row);
+  }
+  pixelCanvas.appendChild(tbody);
 }
